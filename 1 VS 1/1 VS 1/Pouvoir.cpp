c1a6b98b9823bbbf1bfe55cdcf6d0cef107ec8a4
M1n-0 <145598371+M1n-0@users.noreply.github.com>
2023-12-12 11:17:37 +0100

    modif pouvoir.cpp

--- 1 VS 1/1 VS 1/Pouvoir.cpp
+++ 1 VS 1/1 VS 1/Pouvoir.cpp
@@ -2,50 +2,50 @@
 #include <Pouvoir.hpp>
 
 
-Pouvoir::Pouvoir(std::string transformer, std::string absorber, std::string lancer, std::string generer) : m_transformer(transformer), m_absorber(absorber), m_lancer(lancer), m_(generer)
+Pouvoir::Entite(std::string transformer, std::string absorber, std::string lancer, std::string generer) : m_transformer(transformer), m_absorber(absorber), m_lancer(lancer), m_(generer)
 
 
 void Pouvoir::feu()
 {
-
+	std::cout << "Utilisation du pouvoir de feu" << std::endl;
 }
 
 void Pouvoir::eau()
 {
-
+	std::cout << "Utilisation du pouvoir d'eau" << std::endl;
 }
 
 void Pouvoir::electrique()
 {
-
+	std::cout << "Utilisation du pouvoir électrique" << std::endl;
 }
 
 void Pouvoir::tenebre()
 {
-
+	std::cout << "Utilisation du pouvoir des ténèbres" << std::endl;
 }
 
-std::string Pouvoir::get_transformer()
+std::string Entite::get_transformer()
 {
 	return std::string();
 }
 
-std::string Pouvoir::get_absorber()
+std::string Entite::get_absorber()
 {
 	return std::string();
 }
 
-std::string Pouvoir::get_lancer()
+std::string Entite::get_lancer()
 {
 	return std::string();
 }
 
-std::string Pouvoir::get_generer()
+std::string Entite::get_generer()
 {
 	return std::string();
 }
 
-Pouvoir::~Pouvoir()
+Entite::~Entite()
 {
 
 }
